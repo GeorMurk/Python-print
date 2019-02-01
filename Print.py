d34722a78b40e3cac32f49216af8bf3c4f5e81e4
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2019-02-01 19:57:39 +0300

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+#!printing out a simple text
+print("This is a test! typograph.")
+
+#!you can print out a statement on a new line by
+print("the line is new\nthe line is awesome")
+#!and have two lines
+
+#!replace the previous line
+print("the line is old\rthe line is great")
+
+#!Go ahead and test
